90428c6e13381a3fe53fd27c71f8d22fec3878a9
Vitz15 <vitojonathan1515@gmail.com>
2024-11-06 15:42:25 +0800

    Push Tugas Html to JSX

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Clients from "./components/Clients";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <div className="body-wrap">
+      <Header />
+      <main className="site-content">
+        <Hero />
+        <Clients />
+        <Features />
+        <Pricing />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
